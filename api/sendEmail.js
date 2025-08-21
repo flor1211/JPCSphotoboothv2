85539f1ffae77931d85740a5f75cc506c1e7b243
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-08-21 23:55:28 +0800

    fix smtp port

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
 
         const transporter = nodemailer.createTransport({
             host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
+            port: Number(process.env.SMTP_PORT),
             secure: process.env.SMTP_PORT === "465",
             auth: {
                 user: process.env.SMTP_USER, 
