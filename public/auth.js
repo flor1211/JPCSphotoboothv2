581fce274d62bd1e083af6a2bd8a6774228cede7
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-08-23 20:22:52 +0800

    fix some bugs

--- public/auth.js
+++ public/auth.js
@@ -8,29 +8,29 @@
     const data = await res.json();
 
     if (!data.valid) {
-      Swal.fire({
-        icon: "warning",
-        title: "Unauthorized",
-        text: "You must log in to access the photobooth.",
-        timer: 2000,
-        showConfirmButton: false
-      }).then(() => {
+    //   Swal.fire({
+    //     icon: "warning",
+    //     title: "Unauthorized",
+    //     text: "You must log in to access the photobooth.",
+    //     timer: 2000,
+    //     showConfirmButton: false
+    //   }).then(() => {
         window.location.href = "login.html";
-      });
+    //   });
     } else {
         document.getElementById("container").style.display = "block";
     }
   } catch (error) {
     console.error("Token verification error:", error);
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: "An error occurred. Please log in again.",
-      timer: 2000,
-      showConfirmButton: false
-    })
+    // Swal.fire({
+    //   icon: "error",
+    //   title: "Error",
+    //   text: "An error occurred. Please log in again.",
+    //   timer: 2000,
+    //   showConfirmButton: false
+    // })
     // .then(() => {
-    //      window.location.href = "login.html";
+        window.location.href = "login.html";
     // });
   }
 })();
