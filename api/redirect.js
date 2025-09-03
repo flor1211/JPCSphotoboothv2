85224461e44a895d8b19ee94020c98a98dcdc414
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-09-03 23:28:01 +0800

    test toggle mode

--- api/redirect.js
+++ api/redirect.js
@@ -3,10 +3,12 @@ import { join } from "path";
 
 export default function handler(req, res) {
   if (process.env.MAINTENANCE_MODE === "true") {
+
     const file = readFileSync(join(process.cwd(), "maintenance.html"), "utf8");
     res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.status(200).send(file); // serve maintenance.html directly
+    res.status(200).send(file);
   } else {
-    res.status(200).end(); // let Vercel continue and serve index.html
+    res.writeHead(404);
+    res.end();
   }
 }
