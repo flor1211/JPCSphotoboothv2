c791b49b8405cb2cb358adbabdb6a0aea221b608
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-09-03 23:15:02 +0800

    edit redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   if (process.env.MAINTENANCE_MODE === "true") {
-    res.writeHead(302, { Location: "public/maintenance.html" });
+    res.writeHead(302, { Location: "/maintenance.html" });
     res.end();
   } else {
     res.writeHead(200);
