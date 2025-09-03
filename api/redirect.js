a9408eeaad0ac5ddb8d6a26bd8c93e3fc679000d
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-09-04 00:15:41 +0800

    fix bugs redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -3,16 +3,15 @@ import { join } from "path";
 
 export default function handler(req, res) {
 
-    alert(process.env.MAINTENANCE_MODE);
   if (process.env.MAINTENANCE_MODE === "true" || process.env.MAINTENANCE_MODE === true) {
 
-    const file = readFileSync(join(process.cwd(), "maintenance.html"), "utf8");
+    const file = readFileSync(join(process.cwd(), "public", "maintenance.html"), "utf8");
     res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.status(200).send(file);
-    alert("Maintenance mode is enabled. Please try again later.");
+
   } else {
     res.writeHead(404);
     res.end();
-     alert("Maintenance mode is not enabled");
+
   }
 }
