b0242ccf5102265f929807550091e9c2b889194f
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-09-03 22:43:03 +0800

    fix maintenance mode

--- api/maintenance.js
+++ api/maintenance.js
@@ -1,17 +1,9 @@
-import { readFileSync } from "fs";
-import { join } from "path";
-
 export default function handler(req, res) {
-  const maintenance = process.env.MAINTENANCE_MODE === "true";
-
-  if (maintenance) {
-    const filePath = join(process.cwd(), "public", "maintenance.html");
-    const html = readFileSync(filePath, "utf-8");
-
-    res.setHeader("Content-Type", "text/html");
-    return res.status(503).send(html);
+  if (process.env.MAINTENANCE_MODE === "true") {
+    res.writeHead(302, { Location: "/maintenance.html" });
+    res.end();
+  } else {
+    res.writeHead(200);
+    res.end();
   }
-
-  res.setHeader("x-middleware-rewrite", req.url);
-  return res.status(200).end();
 }
