36e2347a8405d1b7f26f64a78d3d611c49be233e
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-09-04 00:32:37 +0800

    fix try

--- api/redirect.js
+++ api/redirect.js
@@ -1,16 +1,14 @@
-import { readFileSync } from "fs";
-import { join } from "path";
-
 export default function handler(req, res) {
-  if (process.env.MAINTENANCE_MODE === "true") {
 
-    const file = readFileSync(join(process.cwd(), "public", "maintenance.html"), "utf8");
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.status(200).send(file);
+  if (process.env.MAINTENANCE_MODE === "true" || process.env.MAINTENANCE_MODE === true) {
+
+    res.writeHead(302, { Location: "/maintenance.html" });
+    res.end();
+
   } else {
+    res.writeHead(302, { Location: "/index.html" });
+    res.end();
 
-    const file = readFileSync(join(process.cwd(), "public", "index.html"), "utf8");
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-    res.status(200).send(file);
   }
 }
+
