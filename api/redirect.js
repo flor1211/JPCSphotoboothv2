316fadff551e6880cd3ffc8e13d47f1c3e29ae10
flor1211 <florenciomendozarosalesjr@gmail.com>
2025-09-04 00:07:51 +0800

    Fix redirect bugs

--- api/redirect.js
+++ api/redirect.js
@@ -2,11 +2,14 @@ import { readFileSync } from "fs";
 import { join } from "path";
 
 export default function handler(req, res) {
-  if (process.env.MAINTENANCE_MODE === "true") {
+    console.log("Maintenance mode:", process.env.MAINTENANCE_MODE);
+
+  if (process.env.MAINTENANCE_MODE === "true" || process.env.MAINTENANCE_MODE === true) {
 
     const file = readFileSync(join(process.cwd(), "maintenance.html"), "utf8");
     res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.status(200).send(file);
+    
   } else {
     res.writeHead(404);
     res.end();
